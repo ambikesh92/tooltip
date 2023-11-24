d8379cf67186205a0d74ca89a6151b56fce9ed01
ambikesh92 <146031699+ambikesh92@users.noreply.github.com>
2023-11-24 15:39:29 -0800

    Update settings.py

--- tooltip/settings.py
+++ tooltip/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jlps)g298w0o%*^r#wzf=h)_j%ol09$syqakdm%wgr%rsq&u@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
